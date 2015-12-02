aa6f971f3cb7ccfc56128ca751ae7bd131751351
Violeta <v.baranovska93@gmail.com>
2015-12-02 17:47:52 +0200

    Added x and y

--- Git_Homework_2/Git_Homework_2/Program.cs
+++ Git_Homework_2/Git_Homework_2/Program.cs
@@ -9,6 +9,28 @@ namespace Git_Homework_2
     {
         static void Main(string[] args)
         {
+            int x, y, counter = 0;
+
+            for (int i = 0; i == counter; i++)
+            {
+                try
+                {
+                    Console.Write("\n Enter please first number:  ");
+                    x = Int32.Parse(Console.ReadLine());
+
+                    Console.Write(" Enter please second number: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("\n " + error.Message);
+                    counter++;
+                }
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
